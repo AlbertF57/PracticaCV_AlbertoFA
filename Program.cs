dddd5a7cb9266ba9d15cbb38970f20206dcbe28b
Alberto <Alberto@DESKTOP-48BJGQH>
2023-06-16 02:32:59 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticaCV_AlbertoFA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
